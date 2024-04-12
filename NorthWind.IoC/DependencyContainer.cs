1dcbf8566395588d5d4e984d7c068f2fce226dfa
Miguel Angel Martinez Gomez <miguelmartinezgomez27@gmail.com>
2024-04-12 03:11:06 -0500

    AppLogger cambios

--- NorthWind.IoC/DependencyContainer.cs
+++ NorthWind.IoC/DependencyContainer.cs
@@ -1,10 +1,14 @@
-﻿namespace Microsoft.Extensions.DependencyInjection;
+﻿using NorthWind.ConsoleApp.Services;
+using NorthWind.Entities.Interfaces;
+
+namespace Microsoft.Extensions.DependencyInjection;
 
 public static class DependencyContainer
 {
-    public static IServiceCollection AddNorthWindServices(this IServiceCollection services)
+    public static IServiceCollection AddServices(this IServiceCollection services)
     {
         services.AddDebugWriter();
+        services.AddServices();
         return services;
     }
 }
